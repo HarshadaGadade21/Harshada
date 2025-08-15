55e0cde5590c5bcded0466059f8986537f3f477f
HarshadaGadade21 <157357956+HarshadaGadade21@users.noreply.github.com>
2025-08-15 16:20:02 +0530

    Add files via upload
    Day 1 challenge

---
+++ day1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main() {
+    int arr[] = {0,1,2,1,0,2,1,0};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int low=0, mid=0, high = n-1;
+
+    while (mid <= high) {
+        if(arr[mid] == 0) {
+            int temp = arr[low];
+            arr[mid] = temp;
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            int temp = arr[mid];
+            arr[mid] = arr[high];
+            arr[high] = temp;
+            high--;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d" , arr[i]);
+
+    return 0;
+}
